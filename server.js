0e37c68532bd1ea44467bcffdd14f24a9527cb7f
Matthew Linden <mlindenx@gmail.com>
2020-08-01 12:23:49 -0500

    note validation added to post route

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(express.json());
 // ===========================================================
 // FUNCTIONS
 // ===========================================================
+
+// This function creates a new note from the post route and pushes the data into the db.json file
 function createNewNote(body, notes) {
   const newNote = body;
   notes.push(newNote);
@@ -24,6 +26,18 @@ function createNewNote(body, notes) {
   return newNote;
 }
 
+function validateNewNote(newNote) {
+  if (!newNote.title || typeof newNote.title !== 'string') {
+    return false;
+  }
+  if (!newNote.text || typeof newNote.text !== 'string') {
+    return false;
+  }
+  if (!newNote.id || typeof animal.id !== 'number') {
+    return false;
+  }
+  return true;
+}
 
 
 // ===========================================================
@@ -45,12 +59,14 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = notes.length.toString();
-
+  // validate the new note data before pushing newNote
+  if (!validateNewNote(req.body)) {
+    res.status(400).send("This note is not properly fromatted.");
+  } else {
   // add new note to db.json file
   const newNote = createNewNote(req.body, notes);
-
   res.json(newNote);
-  console.log(notes)
+  }
 });
 
 
