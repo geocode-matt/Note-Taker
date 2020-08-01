c5809f5fb5ee3352162e0c65f61fcf103283e0cd
Matthew Linden <mlindenx@gmail.com>
2020-08-01 12:09:30 -0500

    post route is pushing newNote to the db.json file

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 // ===========================================================
 // Dependencies
 // ===========================================================
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = 3001;
-const path = require('path');
 const { notes } = require('./db/db.json');
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ===========================================================
+// FUNCTIONS
+// ===========================================================
+function createNewNote(body, notes) {
+  const newNote = body;
+  notes.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notes }, null, 2)
+  );
+  return newNote;
+}
+
 
 
 // ===========================================================
@@ -24,6 +42,29 @@ app.get('/api/notes', (req, res) => {
     console.log(notes);
 });
 
+app.post('/api/notes', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  // add new note to db.json file
+  const newNote = createNewNote(req.body, notes);
+
+  res.json(newNote);
+  console.log(notes)
+});
+
+
+
+
+
+
+
+
+
+
+// ===========================================================
+// LISTENER
+// ===========================================================
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
 });
